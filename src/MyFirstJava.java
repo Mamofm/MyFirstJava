2044db03765516f73acc087b3995314fb4e24d91
Fayben <Fayben@Faybens-MBP.home>
2021-09-20 19:35:33 -0400

    Hello World

---
+++ src/MyFirstJava.java
@@ -0,0 +1,13 @@
+/*
+This the first Java program which print out a message, Hello world.
+
+ */
+public class MyFirstJava {
+    public static void main (String[] args)
+    {
+
+        System.out.println("Hello world");
+
+    }
+    
+}
